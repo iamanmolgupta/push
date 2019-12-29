cc8cf824428f72b5a2914f4759715ebec931ee80
anmol <anmol.gupta@daffodilsw.com>
2019-12-29 21:00:28 +0530

    MostCommented in PPL

--- Schema/imageSchema.js
+++ Schema/imageSchema.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');  //import mongoose
+// var uniqueValidator = require('mongoose-unique-validator');
 //create the schema
 var userSchema = mongoose.Schema({ 
   Email:{type:String},
@@ -6,9 +7,12 @@ var userSchema = mongoose.Schema({
   Image:{type:String},
   Category:{type:String},
   date: { type: String, default: new Date()},
-  enterComment: {type: Array, "default" : []}
+  enterComment: {type: Array, "default" : []},
+  likes :{type: Array, "default" : [], index:true},
+  
 }
 )
 
+// userSchema.index({ likes:1 }, { unique: true });
 //export and model the schema..
 module.exports = mongoose.model('imageSchema',userSchema);
